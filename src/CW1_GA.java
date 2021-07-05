cdbae44b3331e3cbfbc83ce5db3975a67fbbff4a
DominicBeardmore <dominicbeardmore@gmail.com>
2021-07-05 12:02:51 +0100

    Init

--- src/CW1_GA.java
+++ src/CW1_GA.java
@@ -2,9 +2,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Enumeration;
 import java.util.Properties;
+import java.util.Random;
 
+import javax.management.timer.Timer;
 
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
@@ -23,20 +27,30 @@ import io.jenetics.Phenotype;
 import io.jenetics.engine.Codecs;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionStatistics;
+import io.jenetics.stat.DoubleMomentStatistics;
 import io.jenetics.util.DoubleRange;
 
+// Testing was as automated as possible. This programme would run each test 30 times and calculate means.
+// Parameter settings can be set with the global variables
+// There are 4 different test settings
+	// 0 Population and Iteration. The arrays in the popItr method have to be commented out and the total comparison limit modified for each scenario 
+	// 1 Mutation. 10 sets of parameters are created in the mutation method
+	// 2 Crossover. 10 sets of parameters are created in the crossover method
+	// 3 Default runs the parameters set at the top of the file 30 times. The mean of this test is printed
 public class CW1_GA {
 	private static final double A = 10;
 	private static final double R = 5.12;
 	private static final int N = 10;
-
-	private int popSize = 1000;
+	private static final int setting = 3; // 0 PopItr  //1 Mut // 2 crossover // 3 default
+	
+	// SET PARAMETERS HERE
+	private int popSize = 100;
 	private int numSurvivors = 1;
 	private int tournamentSize = 2;
-	private double probMutation = 0.01;
-	private double probCrossover = 0.30;
-	private int numIters = 1000;
-
+	private double probCrossover = 1;
+	private double probMutation = 0.09;
+	private int numIters = 100;
+	
 	private static double fitness(final double[] x) {
 		double value = A*N;
 		for (int i = 0; i < N; ++i) {
@@ -79,7 +93,7 @@ public class CW1_GA {
 		}
 	}
 
-	public void run() {
+	public Double run() {
 		final Engine<DoubleGene, Double> engine = Engine
 			.builder(
 				CW1_GA::fitness,
@@ -91,11 +105,12 @@ public class CW1_GA {
 			.survivorsSelector(new EliteSelector<>(numSurvivors))
 			.offspringSelector(new TournamentSelector<>(tournamentSize))
 			.alterers(
-				//new Mutator<>(probMutation),
-				new GaussianMutator<>(probMutation),
-				//new UniformCrossover<>(probCrossover))
-				new SinglePointCrossover<>(probCrossover))
-				//new MeanAlterer<>(param))
+				new Mutator<>(probMutation),
+//				new GaussianMutator<>(probMutation),
+				//new UniformCrossover<>(probCrossover)
+				new SinglePointCrossover<>(probCrossover) 
+				//new MeanAlterer<>()
+				)
 			.build();
 
 		final EvolutionStatistics<Double, ?>
@@ -106,18 +121,147 @@ public class CW1_GA {
 			.peek(statistics)
 			// Uncomment the following line to get updates at each iteration
 			//.peek(r -> System.out.println(statistics))
+			//.peek(r -> System.out.println(((DoubleMomentStatistics) statistics.getFitness()).getMean()))
 			.collect(toBestPhenotype());
-
-		System.out.println(statistics);
-		System.out.println(best);
+		
+		return best.getFitness();
 	}
 
 
 	public static void main(final String[] args) {
 		CW1_GA alg = new CW1_GA();
-		if(args.length>0) {
-			alg.parseParams(args[0]);
+		double[][] params = new double[10][6];
+		
+			// CREATE TEST PARAMS
+			params = alg.genParams(setting);
+			for (double[] paramSet : params) {
+				alg.popSize = (int) paramSet[0];
+				alg.numSurvivors = (int) paramSet[1];
+				alg.tournamentSize = (int) paramSet[2];
+				alg.probMutation = paramSet[3];
+				alg.probCrossover = paramSet[4];
+				alg.numIters = (int) paramSet[5];
+				alg.runAlg(setting, alg);
+			}
+	}
+	
+	private void runAlg(int setting, CW1_GA alg ) {
+		int repetitions = 30;
+		double[] meanArr = new double [repetitions];
+		double total = 0;
+		long timeElapsed = 0;
+		for (int i = 0; i < repetitions; i++) {
+			Instant start = Instant.now();
+			double best = alg.run();
+			meanArr[i] = best;
+			total += best;
+			Instant finish = Instant.now();
+			timeElapsed += Duration.between(start, finish).toMillis();
 		}
-		alg.run();
+
+		System.out.println("--------------------------------------------------");
+		printParams(setting);
+		System.out.println("Mean " + total / repetitions);
+		System.out.println("Time " + timeElapsed / repetitions);
+		System.out.println("--------------------------------------------------");
+	}
+	
+	private void printMut() {
+		System.out.println("Probability of Mutation: " + probMutation );
+	}
+	
+	private void printCrossover() {
+		System.out.println("Probability of Crossover: " + probCrossover );
+	}
+	
+	private void printPop() {
+		System.out.println("Population: " + popSize );
+	}
+	
+	private void printItr() {
+		System.out.println("Iterations: " + numIters );
+	}
+	
+	private void printTournamentSize() {
+		System.out.println("Tournament Size: " + tournamentSize );
+	}
+	
+	private void printSurvivors() {
+		System.out.println("Number of Survivors: " + numSurvivors );
+	}
+	
+	private void printParams(int setting) {
+		
+		printMut();
+		printCrossover();
+		printPop();
+		printItr();
+		printTournamentSize();
+		printSurvivors();
+		
+	}
+
+	private double[][] genParams(int param) {
+		double[][] params = new double[10][6];
+		switch (param) {
+			case 0: 
+				params = popItr();
+				break;
+			case 1: 
+				params = mutation();
+				break;
+			case 2: 
+				params = crossover();
+				break;
+			case 3: 
+				params = new double[][]{{popSize, numSurvivors, tournamentSize, probMutation,probCrossover, numIters}};
+				break;
+			default:
+				
+				
+		}
+		return params;
+	}
+	
+	// Generates params for population and iteration
+	private double[][] popItr() {
+		int[] popArr = {50, 100, 200, 500, 1000, 2000, 4000, 5000, 10000, 20000}; // 1000000
+//		int[] popArr = {40, 50, 80, 100, 125, 200, 250, 400, 500, 1000}; // 10000
+		int totalComp = 1000000; // Total comparisons, needs to be changed for each scenario either 100,000 or 10,000
+		double[][] params = new double[10][6];
+		for (int i = 0; i < popArr.length; i++) {
+			int itr = (totalComp / popArr[i]);
+			params[i] = new double[]{popArr[i], numSurvivors, tournamentSize, probMutation, probCrossover, itr};
+		}
+		
+		return params;
+	}
+	
+	// Generates params for the mutation tests
+	private double[][] mutation() {
+		double[][] params = new double[10][6];
+		// Changes the search increment
+		double modFac = 0.01;
+		for (int i = 0; i <= 9; i++) {
+			double mut = (i * modFac);
+			// To test a higher range, add to the mut value before putting into the params array
+			params[i] = new double[]{popSize, numSurvivors, tournamentSize, mut, probCrossover, numIters};
+		}
+		
+		return params;
+	}
+	
+	// Generates params for the crossover tests
+	private double[][] crossover() {
+		double[][] params = new double[10][6];
+		// Changes the search increment
+		double crossProb = 0.1;
+		for (int i = 0; i <= 9; i++) {
+			// To test a higher range, add to the cross value before putting into the params array
+			double cross = (i * crossProb);
+			params[i] = new double[]{popSize, numSurvivors, tournamentSize, probMutation, cross, numIters};
+		}
+		
+		return params;
 	}
 }
--- src/CW1_PSO.java
+++ src/CW1_PSO.java
@@ -10,17 +10,31 @@ import net.sourceforge.jswarm_pso.FitnessFunction;
 import net.sourceforge.jswarm_pso.Particle;
 import net.sourceforge.jswarm_pso.Swarm;
 import java.lang.Math;
+import java.time.Duration;
+import java.time.Instant;
 
 public class CW1_PSO {
+	// There are 6 settings to run all the different tests. The numbers are the position in the param array.
+		// 0 Population and iteration
+		// 2 Neighbourhood weight
+		// 3 Inertia Weight
+		// 4 Personal Weight
+		// 5 Global Weight
+		// 6 Velocity
+	// There is no default setting that only runs the params set. However PSO is quick enough that this was not an issue to wait a short time for individual when required
+	
 	private static double R = 5.12;
 
-	private int numParticles = 1000;
-	private int numIters = 1000;
-	private double neighWeight = 1.0;
-	private double inertiaWeight = 1.0;
-	private double personalWeight = 1.0;
-	private double globalWeight = 1.0;
-	private double maxMinVelocity = 0.0001;
+	private static final int setting = 6; // 0 PopItr  // 2 Neighweight // 3 IW // 4 Personal Weight // 5 GW // 6 Velocity
+	
+	// SET PARAMETERS HERE
+	private int numParticles = 80;
+	private int numIters = 125;
+	private double neighWeight = 0.25;
+	private double inertiaWeight = 0.5;
+	private double personalWeight = 2.25;
+	private double globalWeight = 0.5;
+	private double maxMinVelocity = 0.04;
 
 	public void parseParams(String paramFile) {
 		try {
@@ -57,7 +71,7 @@ public class CW1_PSO {
 		}
 	}
 
-	public void run() {
+	public double run() {
 		// Create a swarm (using 'MyParticle' as sample particle 
 		// and 'MyFitnessFunction' as finess function)
 		Swarm swarm = new Swarm(numParticles
@@ -88,14 +102,170 @@ public class CW1_PSO {
 			swarm.evolve();
 			//System.out.println(swarm.toStringStats());
 		}
-		System.out.println(swarm.toStringStats());
+		
+		return swarm.getBestFitness();
 	}
 
 	public static void main(final String[] args) {
 		CW1_PSO alg = new CW1_PSO();
+		double[][] params = new double[10][7];
 		if(args.length>0) {
 			alg.parseParams(args[0]);
 		}
-		alg.run();
+		
+		params = alg.genParams();
+		
+		for (double[] paramSet : params) {
+			alg.numParticles 	= (int) paramSet[0];
+			alg.numIters 		= (int) paramSet[1];
+			alg.neighWeight 	= paramSet[2];
+			alg.inertiaWeight 	= paramSet[3];
+			alg.personalWeight 	= paramSet[4];
+			alg.globalWeight 	= paramSet[5];
+			alg.maxMinVelocity 	= paramSet[6];
+			alg.runAlg(setting, alg);
+		}
 	}
+	
+	private double[][] genParams() {
+
+		double[][] params = stdParams();
+		switch (setting) {
+			case 0: // Population Iteration
+				params = popItr();
+				break;
+			case 2: // neighboohood weight
+				params = nw(params, neighWeight);
+				break;
+			case 3: // Inertia Weight
+				params = nw(params, inertiaWeight);
+				break;
+			case 4: // Personal weight
+				params = nw(params, personalWeight);
+				break;
+			case 5: // Global Weight
+				params = nw(params, globalWeight);
+				break;
+			case 6: // Velocity
+				params = nw(params, maxMinVelocity);
+				break;
+			default:
+		}
+		return params;
+	}
+	
+	private double[][] stdParams(){
+		double[][] params = new double[10][6];
+		for (int i = 0; i < 10; i++) {
+			params[i] = new double[]{
+					numParticles,
+					numIters,
+					neighWeight,
+					inertiaWeight,
+					personalWeight,
+					globalWeight,
+					maxMinVelocity
+					};
+		}
+		
+		return params;
+	}
+
+	// Generates params for population and iteration
+	private double[][] popItr() {
+//		int[] pop = {50, 100, 200, 500, 1000, 2000, 4000, 5000, 10000, 20000}; // 1000000
+		int[] pop = {40, 50, 80, 100, 125, 200, 250, 400, 500, 1000}; // 10000
+		int totalComp = 10000; // Total comparisons, needs to be changed for each scenario either 100,000 or 10,000
+		double[][] params = new double[10][6];
+		for (int i = 0; i < pop.length; i++) {
+			int itr = (totalComp / pop[i]);
+			params[i] = new double[]{pop[i], itr, neighWeight, inertiaWeight, personalWeight, globalWeight, maxMinVelocity };
+		}
+		
+		return params;
+	}
+	
+	// Modifies 1 param based on the setting value
+	private double[][] nw(double[][] params, double param) {
+		double[] nw = modParam(param);
+
+		for (int i = 0; i < 10; i++) {
+			params[i][setting] = nw[i];
+		}
+
+		return params;
+	}
+	
+	private double[] modParam(double param) {
+		double[] modParam = new double[10];
+		double modFac = 5; // Increment size
+
+		for (int i = 0; i <= 9; i++) {
+			double nw = (i * modFac);
+			modParam[i] = nw;
+		}
+		
+		return modParam;
+	}
+
+	private void runAlg(int setting, CW1_PSO alg) {
+		int repetitions = 30;
+		double[] meanArr = new double [repetitions];
+		double total = 0;
+		long timeElapsed = 0;
+
+		for (int i = 0; i < repetitions; i++) {
+			Instant start = Instant.now();
+			double best = alg.run();
+			meanArr[i] = best;
+			total += best;
+			Instant finish = Instant.now();
+			timeElapsed += Duration.between(start, finish).toMillis();
+		}
+		System.out.println("--------------------------------------------------");
+		printParams(setting);
+		System.out.println("Mean " + total / repetitions);
+		System.out.println("Time " + timeElapsed / repetitions);
+		System.out.println("--------------------------------------------------");
+		
+	}
+	
+	private void printParams(int setting) {
+		printParticles();
+		printIter();
+		printNeighW();
+		printInertiaWeight();
+		printPersW();
+		printGlobalWeight();
+		printVelocity();
+	}
+
+	private void printParticles() {
+		System.out.println("Number of Particles: " + numParticles );
+	}
+	
+	private void printIter() {
+		System.out.println("Number of Iterations: " + numIters );
+	}
+	
+	private void printNeighW() {
+		System.out.println("neighbourhood weight " + neighWeight );
+	}
+	
+	private void printInertiaWeight() {
+		System.out.println("Inertia weight: " + inertiaWeight );
+	}
+	
+	private void printPersW() {
+		System.out.println("Personal Weight: " + personalWeight );
+	}
+	
+	private void printGlobalWeight() {
+		System.out.println("Global Weight: " + globalWeight );
+	}
+	
+	private void printVelocity() {
+		System.out.println("Velocity: " + maxMinVelocity );
+	}
+	
 }
